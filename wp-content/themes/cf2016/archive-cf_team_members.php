aface5ac3090fcbfe5ed7c09a04001889a7824f0
Michele Rose <ldnallen@gmail.com>
2016-08-04 16:56:14 -0700

    Add styles, social buttons, tweaks to archive template

--- wp-content/themes/cf2016/archive-cf_team_members.php
+++ wp-content/themes/cf2016/archive-cf_team_members.php
@@ -29,72 +29,61 @@ get_header(); ?>
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header>
+			<section class="team_container">
 
 			<?php
-			
 			while ( have_posts() ) : the_post(); // Start the Loop.
-
-				the_post();
 
 				// Get 'team' posts
 				$team_posts = get_posts( array(
 					'post_type' => 'cf_team_members',
-					'posts_per_page' => 12, // Unlimited posts
+					'posts_per_page' => 12, // 12 posts per page
 					'orderby' => 'title', // Order alphabetically by name
 				) );
 
 				if ( $team_posts ):
 				?>
-				<section class="row profiles">
 					
 					<?php 
-					foreach ( $team_posts as $post ): 
-					setup_postdata($post);
-					$position = get_post_meta( get_the_ID(), '_cf_team_position', true );
-					$facebook = get_post_meta( get_the_ID(), '_cf_team_facebook', true );
-					$twitter = get_post_meta( get_the_ID(), '_cf_team_twitter', true );
+						$position = get_post_meta( get_the_ID(), '_cf_team_position', true );
+						$facebook = get_post_meta( get_the_ID(), '_cf_team_facebookurl', true );
+						$twitter = get_post_meta( get_the_ID(), '_cf_team_twitterurl', true );
 					?>
-					<article class="col-md-4 profile">
-						<div class="profile-header">
-							<?php if ( has_post_thumbnail() ): ?>
-								<?php echo get_the_post_thumbnail('', 'team_thumb', array( 'title' => get_the_title() )); ?>
-							<?php endif; ?>
-						</div>
-						
-						<div class="profile-content">
-							<h3><?php the_title(); ?></h3>
-							<p class="lead position"><?php echo $position; ?></p>
-							<?php the_content(); ?>
-						
-							<div class="profile-footer">
-								<?php if ( !is_null($facebook) ): ?>
-								<a href="<?php echo $facebook; ?>"><i class="icon-facebook"></i></a>
-								<?php endif; ?>
-								<?php if ( !is_null($twitter) ): ?>
-								<a href="<?php echo $twitter; ?>"><i class="icon-twitter"></i></a>
+					<article class="col-md-4 team">
+						<div class="team_wrapper">
+							<div class="team_header">
+								<?php if ( has_post_thumbnail() ): ?>
+									<?php echo get_the_post_thumbnail('', 'team_thumb', array( 'title' => get_the_title() )); ?>
 								<?php endif; ?>
 							</div>
-						</div>
-					</article><!-- /.profile -->
-					<?php endforeach; ?>
-				</section><!-- /.row -->
+							
+							<div class="team_content">
+								<div class="team_inner_center">
+									<h3><?php the_title(); ?></h3>
+									<p class="position"><?php echo $position; ?></p>
+									<div class="social_links">
+										<?php if ( !is_null($facebook) ): ?>
+										<a class="btn btn-social-icon btn-facebook" href="<?php echo $facebook; ?>"><span class="fa fa-facebook"></span></a>
+										<?php endif; ?>
+										<?php if ( !is_null($twitter) ): ?>
+										<a class="btn btn-social-icon btn-twitter" href="<?php echo $twitter; ?>"><span class="fa fa-twitter"></span></a>
+										<?php endif; ?>
+									</div>
+									<button class="more_button btn btn-primary">Read More</button>
+								</div>	
+								<div class="the_content">
+									<?php the_content(); ?>
+									<button class="less_button btn btn-primary">Read Less</button>
+								</div>
+							</div>
+						</div>	
+					</article><!-- /.team -->
+				
 				<?php endif;
 
-			
-			endwhile; // End the loop.
-
-			// Previous/next page navigation.
-			/*the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-			) );*/
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+		endwhile; // End the loop. ?>
+		</section><!-- /.row -->
+		<?php endif;
 		?>
 
 		</main><!-- .site-main -->
--- wp-content/themes/cf2016/functions.php
+++ wp-content/themes/cf2016/functions.php
@@ -14,6 +14,19 @@ function enqueue_parent_styles() {
 add_action('wp_enqueue_scripts', 'enqueue_bootstrap_styles' );
 function enqueue_bootstrap_styles(){
 	wp_enqueue_style( 'bootstrap-style', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+	wp_enqueue_style( 'bootstrap-social', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.0.0/bootstrap-social.min.css');
+}
+
+// Add fontawesome styles
+add_action('wp_enqueue_scripts', 'enqueue_fontawesome_styles' );
+function enqueue_fontawesome_styles(){
+	wp_enqueue_style( 'fontawesome_styles', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css');
+}
+
+// Add global js file
+add_action('wp_enqueue_scripts', 'cf_global_js');
+function cf_global_js() {
+    wp_enqueue_script( 'global_js', get_stylesheet_directory_uri() . '/js/global.js', array( 'jquery' ), '1.0', true );
 }
 
 // Custom Image Sizes
